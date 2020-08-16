67793171409c02101fedf4d5a2b80deb9b322b4e
gubiarpa <billy.arredondo@pucp.pe>
2020-08-16 03:18:24 -0500

    Uso de *ngIf, *ngFor
    Se hace uso de las etiquetas.

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -5,5 +5,13 @@ import { Component } from '@angular/core';
     templateUrl: './body.component.html'
 })
 export class BodyComponent {
+    mostrar = false; // inicialización en true
+    
+    frase: any = {
+        mensaje: 'Un gran poder conlleva una gran responsabilidad',
+        autor: 'Ben Parker'
+    }
+
+    personajes: string[] = ['Spiderman', 'Venom', 'Octopus', 'Scorpion', 'Rhino']
 
 }
